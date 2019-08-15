bb7cb8d6495fadfc232e8f6d5da5f42b5c48db0e
tosingithub <adewumi_tosin@hotmail.com>
2019-08-15 09:53:22 +0200

    added wip remote

--- sdeskmodel.py
+++ sdeskmodel.py
@@ -7,3 +7,10 @@
 
 if __name__ == "__main__":
     print("Desk")
+    print("Done")
+    print("Final")
+    print("2nd day")
+
+    print("2nd 2nd comment")
+    print("new branch")
+    print("from wip remote")
